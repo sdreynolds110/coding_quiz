31b98857100c8da483b53db1ae5c0c89d6d753c2
Stephen Reynolds <sdreynolds110@gmail.com>
2019-12-02 19:10:38 -0500

    making initial edits

---
+++ questions.js
@@ -0,0 +1,15 @@
+var questions = [
+    {
+    title: "Is this coding bootcamp hard?",
+    choices: ["no", "yes", "this is the hardest thing I've ever done in my life"],
+    answer: "this is the hardest thing I've ever done in my life"
+    },
+    {
+    title: "What file do you use to style your website?",
+    choices: ["html", "css", "stylesheet", "javascript"],
+    answer: "css"
+    },
+]
+var score = 0;
+
+for(var i = 0; i < questions.length; i++)
